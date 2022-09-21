3e013b92f9675e19da3a2c786f174bca6fa050da
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-09-21 15:27:49 +0400

    rw interface basics

--- src/builder.rs
+++ src/builder.rs
@@ -2,66 +2,123 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-macro_rules! fnd {
-	($Name:ident: $ty:tt) => {
-		fn $ty(self, val: $ty) -> $Name<Self>
-		where
-			Self: Sized,
-		{
-			$Name::new(self, val)
-		}
-	};
+use crate::reader::*;
+use crate::writer::*;
+
+pub struct Nil;
+
+pub trait ToRwInterface {
+	fn rw<I>(&self) -> I
+	where
+		I: RwInterface<Tail = Nil, Head = Self>,
+		Self: Sized;
+
+	fn to_rw<T: RwInterface, I>(&self, rwi: T) -> I
+	where
+		I: RwInterface<Head = Self, Tail = T>,
+		Self: Sized;
+
+	fn from_rwi<I>(rwi: I) -> Self
+	where
+		I: RwInterface<Head = Self>,
+		Self: Sized,
+	{
+		*rwi.head()
+	}
 }
 
-pub trait Rw {
-	fnd!(Bool: bool);
-	fnd!(Char: char);
+pub trait RwInterface {
+	/// The head is the type of the last value added.
+	///
+	/// This is the type that created this [`RwInterface`] in particular.
+	type Head;
+	/// The tail is the type of all the other values added.
+	type Tail;
 
-	fnd!(U8: u8);
-	fnd!(U16: u16);
-	fnd!(U32: u32);
-	fnd!(U64: u64);
-	fnd!(U128: u128);
+	fn head(&self) -> &Self::Head;
+	fn tail(&self) -> &Self::Tail;
 
-	fnd!(I8: i8);
-	fnd!(I16: i16);
-	fnd!(I32: i32);
-	fnd!(I64: i64);
-	fnd!(I128: i128);
+	fn take(self) -> (Self::Head, Self::Tail)
+	where
+		Self: Sized,
+	{
+		(*self.head(), *self.tail())
+	}
+
+	fn new(tail: Self::Tail, head: Self::Head) -> Self;
+
+	fn to(self, tail: Self::Tail) -> Self
+	where
+		Self: Sized,
+	{
+		Self::new(tail, *self.head())
+	}
 }
 
-macro_rules! foundational {
-	($Name:ident: $ty:ty) => {
-		pub struct $Name<RW>
-		where
-			RW: Rw,
-		{
-			pub(crate) rw: RW,
-			pub value: $ty,
-		}
-
-		impl<RW> $Name<RW>
-		where
-			RW: Rw,
-		{
-			pub fn new(rw: RW, value: $ty) -> Self {
-				Self { rw, value }
-			}
-		}
-	};
+pub struct Val<Head, Tail> {
+	pub value: Head,
+	tail: Tail,
 }
 
-foundational!(Bool: bool);
-foundational!(Char: char);
+impl<Head, Tail> RwInterface for Val<Head, Tail> {
+	type Head = Head;
+	type Tail = Tail;
 
-foundational!(U8: u8);
-foundational!(U16: u16);
-foundational!(U32: u32);
-foundational!(U64: u64);
-foundational!(U128: u128);
+	fn new(tail: Tail, head: Head) -> Self {
+		Self { value: head, tail }
+	}
 
-foundational!(I8: i8);
-foundational!(I16: i16);
-foundational!(I32: i32);
-foundational!(I64: i64);
-foundational!(I128: i128);
+	fn head(&self) -> &Head {
+		&self.value
+	}
+
+	fn tail(&self) -> &Tail {
+		&self.tail
+	}
+}
+
+pub trait DeserializeWith<Context, T = Self> {
+	fn read_with(reader: &mut impl Reader, context: Context) -> T;
+}
+
+pub trait Serialize {
+	fn write(self, writer: &mut impl Writer);
+}
+
+pub trait Deserialize<T = Self> {
+	fn read(reader: &mut impl Reader) -> T;
+}
+
+impl<T> Serialize for T
+where
+	T: RwInterface,
+	T::Head: Serialize,
+	T::Tail: Serialize,
+{
+	fn write(self, writer: &mut impl Writer) {
+		self.tail().write(writer);
+		self.head().write(writer);
+	}
+}
+
+impl<T> Deserialize for T
+where
+	T: RwInterface,
+	T::Head: Deserialize,
+	T::Tail: Deserialize,
+{
+	fn read(reader: &mut impl Reader) -> Self {
+		Self::new(T::Tail::read(reader), T::Head::read(reader))
+	}
+}
+
+impl<T, Context> DeserializeWith<Context> for T
+where
+	T: RwInterface,
+	T::Head: DeserializeWith<Context>,
+	T::Tail: Deserialize,
+{
+	fn read_with(reader: &mut impl Reader, context: Context) -> Self {
+		Self::new(T::Tail::read(reader), T::Head::read_with(reader, context))
+	}
+}
