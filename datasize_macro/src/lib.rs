3549d934b8ac0908bbb3a104bdcd937c3e93a2e3
Syudagye <syudagye@gmail.com>
2022-11-27 12:14:39 +0100

    Added license headers to all source files  + Added infos in `datasize_macro`'s `Cargo.toml`

--- datasize_macro/src/lib.rs
+++ datasize_macro/src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use impl_data_sizes::{
 	impl_datasize, impl_static_data_size, retrieve_generics, retrieve_lifetimes,
 };
