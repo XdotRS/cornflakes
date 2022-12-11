8abf290ecbfe5aa7f8d8e07c3278cea0c6041108
Syudagye <syudagye@gmail.com>
2022-12-11 21:22:02 +0100

    Rewrote integration tests for `Readable` derive

--- tests/readable_derive_test.rs
+++ tests/readable_derive_test.rs
@@ -1,110 +1,182 @@
-use cornflakes::derive::Readable;
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#[derive(Readable)]
-enum TestEnum {
+#![allow(incomplete_features)]
+#![feature(specialization)]
+
+use cornflakes::derive::{DataSize, Readable, StaticDataSize};
+
+#[derive(Debug, PartialEq, Eq, Readable, StaticDataSize)]
+enum TestEnumWrapper {
 	Unit,
 	Unnamed(u16),
 }
 
-#[derive(Readable)]
-enum TestEnumGenerics<T> {
+// for reference
+//
+// impl cornflakes::Readable for TestEnumWrapper {
+// 	fn read_from(reader: &mut impl bytes::Buf) -> cornflakes::ReadResult<Self> {
+// 		let __0__ = <u16 as cornflakes::Readable>::read_from(reader)?;
+// 		Ok(match __0__ {
+// 			0 => Self::Unit,
+// 			v => Self::Unnamed(v),
+// 		})
+// 	}
+// }
+
+// Is it really possible to do it here ? or do we do it manually ?
+#[derive(Debug, PartialEq, Eq, Readable, DataSize, StaticDataSize)]
+enum TestEnumWrapperGenerics<T> {
 	Unit,
 	Unnamed(T),
 }
 
-#[derive(Readable)]
-enum TestNamedEnum {
-	Unit,
-	Named { field1: u8, field2: u8 },
+// for reference
+//
+// impl<T: cornflakes::Readable + cornflakes::StaticDataSize> cornflakes::Readable for TestEnumWrapperGenerics<T> {
+// 	fn read_from(reader: &mut impl bytes::Buf) -> cornflakes::ReadResult<Self> {
+// 		let datasize = <T as cornflakes::StaticDataSize>::static_data_size();
+// 		let raw: &[u8] = reader.take(datasize).get_u8;
+// 		let __0__ = <T as cornflakes::Readable>::read_from(reader)?;
+// 		Ok(match __0__ {
+// 			0 => Self::Unit,
+// 			v => Self::Unnamed(v),
+// 		})
+// 	}
+// }
+
+#[derive(Debug, PartialEq, Eq, StaticDataSize, Readable)]
+#[repr(u8)]
+enum TestEnumUnits {
+	Variant0,
+	Variant1,
+	Variant2,
+	Variant3,
+	Variant4,
+	Variant5,
 }
 
-#[derive(Readable)]
-enum TestNamedEnumGenerics<T> {
-	Unit,
-	Named { field1: T, field2: u8 },
-}
+// for reference
+//
+// impl cornflakes::Readable for TestEnumUnits {
+// 	fn read_from(reader: &mut impl bytes::Buf) -> cornflakes::ReadResult<Self> {
+// 		let __0__ = <u8 as cornflakes::Readable>::read_from(reader)?;
+// 		Ok(match __0__ {
+// 			0 => Self::Variant0,
+// 			1 => Self::Variant1,
+// 			2 => Self::Variant2,
+// 			4 => Self::Variant3,
+// 			5 => Self::Variant4,
+// 			6 => Self::Variant5,
+// 		})
+// 	}
+// }
 
-#[derive(Readable)]
+#[derive(Debug, PartialEq, Eq, DataSize, StaticDataSize, Readable)]
 struct TestStruct<T> {
 	value: i32,
 	value_generic: T,
 	wrapper: Option<u8>,
 	wrapper_generic: Option<T>,
-	enum_value: TestEnum,
-	enum_generic_value: TestEnumGenerics<T>,
+	enum_value: TestEnumWrapper,
+	enum_generic_value: TestEnumWrapperGenerics<T>,
 }
 
-#[derive(Readable)]
+// for reference
+//
+// impl<T: cornflakes::Readable> cornflakes::Readable for TestStruct<T> {
+// 	fn read_from(reader: &mut impl bytes::Buf) -> cornflakes::ReadResult<Self> {
+// 		let __value__ = <i32 as cornflakes::Readable>::read_from(reader)?;
+// 		let __value_generic__ = <T as cornflakes::Readable>::read_from(reader)?;
+// 		let __wrapper__ = <Option<u8> as cornflakes::Readable>::read_from(reader)?;
+// 		let __wrapper_generic__ = <Option<T> as cornflakes::Readable>::read_from(reader)?;
+// 		let __enum_value__ = <TestEnumWrapper as cornflakes::Readable>::read_from(reader)?;
+// 		let __enum_generic_value__ =
+// 			<TestEnumWrapperGenerics<T> as cornflakes::Readable>::read_from(reader)?;
+// 		Ok(Self {
+// 			value: __value__,
+// 			value_generic: __value_generic__,
+// 			wrapper: __wrapper__,
+// 			wrapper_generic: __wrapper_generic__,
+// 			enum_value: __enum_value__,
+// 			enum_generic_value: __enum_generic_value__,
+// 		})
+// 	}
+// }
+
+#[derive(Debug, PartialEq, Eq, DataSize, StaticDataSize, Readable)]
 struct TestTuple<T>(u32, T, Option<i64>, Option<T>, TestStruct<T>);
+
+// for reference
+//
+// impl<T: cornflakes::Readable> cornflakes::Readable for TestTuple<T> {
+// 	fn read_from(reader: &mut impl bytes::Buf) -> cornflakes::ReadResult<Self> {
+// 		let __0__ = <u32 as cornflakes::Readable>::read_from(reader)?;
+// 		let __1__ = <T as cornflakes::Readable>::read_from(reader)?;
+// 		let __2__ = <Option<i64> as cornflakes::Readable>::read_from(reader)?;
+// 		let __3__ = <Option<T> as cornflakes::Readable>::read_from(reader)?;
+// 		let __4__ = <TestStruct<T> as cornflakes::Readable>::read_from(reader)?;
+// 		Ok(Self(__0__, __1__, __2__, __3__, __4__))
+// 	}
+// }
 
 // --=+=--
 //  Tests
 // --=+=--
 
-// Simple Enums
+// Wrapper Enums
 
 #[test]
-fn test_readable_enum_unit() {
+fn test_readable_wrapper_enum_unit() {
 	let data: &[u8] = &b"\x00\x00"[..];
-	let r#enum = TestEnum::Unit;
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#enum);
+	let r#enum = TestEnumWrapper::Unit;
+	assert_eq!(
+		<TestEnumWrapper as cornflakes::Readable>::read_from(&mut data).unwrap(),
+		r#enum
+	);
 }
 
 #[test]
-fn test_readable_enum_unnamed() {
+fn test_readable_wrapper_enum_unnamed() {
 	let data: &[u8] = &b"\x00\xFF"[..];
-	let r#enum = TestEnum::Unnamed(0x00FF);
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#enum);
+	let r#enum = TestEnumWrapper::Unnamed(0x00FF);
+	assert_eq!(
+		<TestEnumWrapper as cornflakes::Readable>::read_from(&mut data).unwrap(),
+		r#enum
+	);
 }
 
 #[test]
-fn test_readable_enum_gereric_unit() {
+fn test_readable_wrapper_enum_gereric_unit() {
 	let data: &[u8] = &b"\x00\x00\x00\x00"[..];
-	let r#enum = TestEnumGenerics::Unit::<i32>;
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#enum);
+	let r#enum = TestEnumWrapperGenerics::Unit::<i32>;
+	assert_eq!(
+		<TestEnumWrapperGenerics<i32> as cornflakes::Readable>::read_from(&mut data).unwrap(),
+		r#enum
+	);
 }
 
 #[test]
-fn test_readable_enum_gereric_unnamed() {
+fn test_readable_wrapper_enum_gereric_unnamed() {
 	let data: &[u8] = &b"\x00\x02\xAB\x98"[..];
-	let r#enum = TestEnumGenerics::Unnamed::<u32>(175000);
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#enum);
+	let r#enum = TestEnumWrapperGenerics::Unnamed::<u32>(175000);
+	assert_eq!(
+		<TestEnumWrapperGenerics<u32> as cornflakes::Readable>::read_from(&mut data).unwrap(),
+		r#enum
+	);
 }
 
-// Named Enums
+// Unit Enums
 
 #[test]
-fn test_readable_named_enum_unit() {
-	let data: &[u8] = &b"\x00\x00"[..];
-	let r#enum = TestNamedEnum::Unit;
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#enum);
-}
-
-#[test]
-fn test_readable_named_enum_named() {
-	let data: &[u8] = &b"\x00\xFF"[..];
-	let r#enum = TestNamedEnum::Named {
-		field1: 0,
-		field2: 0xFF,
-	};
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#enum);
-}
-
-#[test]
-fn test_readable_named_enum_gereric_unit() {
-	let data: &[u8] = &b"\x00\x00\x00"[..];
-	let r#enum = TestNamedEnumGenerics::Unit::<i16>;
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#enum);
-}
-
-#[test]
-fn test_readable_named_enum_gereric_named() {
-	let data: &[u8] = &b"\x00\x00\x02\xAB\x98"[..];
-	let r#enum = TestNamedEnumGenerics::Named::<u32> {
-		field1: 0x2AB,
-		field2: 0x98,
-	};
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#enum);
+fn test_readable_unit_enum() {
+	let data: &[u8] = &b"\x04"[..];
+	let r#enum = TestEnumUnits::Variant4;
+	assert_eq!(
+		<TestEnumUnits as cornflakes::Readable>::read_from(&mut data).unwrap(),
+		r#enum
+	);
 }
 
 // Structs
@@ -117,10 +189,13 @@ fn test_readable_struct() {
 		value_generic: 48,
 		wrapper: None,
 		wrapper_generic: Some(32),
-		enum_value: TestEnum::Unit,
-		enum_generic_value: TestEnumGenerics::Unit,
+		enum_value: TestEnumWrapper::Unit,
+		enum_generic_value: TestEnumWrapperGenerics::Unit,
 	};
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#struct);
+	assert_eq!(
+		<TestStruct<u8> as cornflakes::Readable>::read_from(&mut data).unwrap(),
+		r#struct
+	);
 }
 
 #[test]
@@ -131,8 +206,11 @@ fn test_readable_tuple() {
 		value_generic: 48,
 		wrapper: None,
 		wrapper_generic: Some(32),
-		enum_value: TestEnum::Unit,
-		enum_generic_value: TestEnumGenerics::Unit,
+		enum_value: TestEnumWrapper::Unit,
+		enum_generic_value: TestEnumWrapperGenerics::Unit,
 	};
-	assert_eq!(TestEnum::read_from(data).unwrap(), r#struct);
+	assert_eq!(
+		<TestStruct<u8> as cornflakes::Readable>::read_from(&mut data).unwrap(),
+		r#struct
+	);
 }
